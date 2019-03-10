d8bfb6114daf4400e593c2558160de5eb42f1b34
ArturJS <nizamutdin.art@gmail.com>
2019-03-10 21:46:52 +0400

    Added simple unit-test for sleep.js util

---
+++ src/utils/__tests__/sleep.test.js
@@ -0,0 +1,37 @@
+import sleep from '../sleep';
+
+const flushPromises = () => new Promise(resolve => setImmediate(resolve));
+
+describe('Check `sleep` util', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  it('should NOT be resolved before time is up', async () => {
+    let isDone = false;
+    const timeMs = 5000;
+
+    sleep(timeMs).then(() => {
+      isDone = true;
+    });
+
+    jest.advanceTimersByTime(timeMs - 1);
+    await flushPromises();
+
+    expect(isDone).toBe(false);
+  });
+
+  it('should be resolved after time is up', async () => {
+    let isDone = false;
+    const timeMs = 5000;
+
+    sleep(timeMs).then(() => {
+      isDone = true;
+    });
+
+    jest.advanceTimersByTime(timeMs);
+    await flushPromises();
+
+    expect(isDone).toBe(true);
+  });
+});
