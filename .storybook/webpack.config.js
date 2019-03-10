42268585cc260aee1fc6cf542792ed85d6ecfcb7
ArturJS <nizamutdin.art@gmail.com>
2019-03-10 11:35:35 +0400

    Replace default react-modal classNames with custom 'rmb-*'

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,34 @@
 const path = require('path');
 
+const toMultipleReplaceQueries = obj =>
+  Object.entries(obj).map(([key, value]) => ({
+    search: key,
+    replace: value
+  }));
+
+const reactModalClassNames = {
+  ReactModalPortal: 'rmb-portal',
+  'ReactModal__Body--open': 'rmb-body--open',
+  ReactModal__Overlay: 'rmb-overlay',
+  ReactModal__Content: 'rmb-content'
+};
+
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader'],
-                include: path.resolve(__dirname, '../')
-            }
-        ]
-    }
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../')
+      },
+      {
+        test: /react-modal.*\.js$/,
+        include: /node_modules/,
+        loader: 'string-replace-loader',
+        options: {
+          multiple: toMultipleReplaceQueries(reactModalClassNames)
+        }
+      }
+    ]
+  }
 };
