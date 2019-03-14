327287b00001e773bbb25f71c555a8088f5b4c87
ArturJS <nizamutdin.art@gmail.com>
2019-03-14 22:52:56 +0400

    Added basis for Nightwatch.js + Browserstack

---
+++ e2e-tests/conf/parallel.conf.js
@@ -0,0 +1,56 @@
+nightwatch_config = {
+  src_folders: ['tests/single'],
+
+  selenium: {
+    start_process: false,
+    host: 'hub-cloud.browserstack.com',
+    port: 80
+  },
+
+  common_capabilities: {
+    build: 'nightwatch-browserstack',
+    'browserstack.user':
+      process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+    'browserstack.key':
+      process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+    'browserstack.debug': true
+  },
+
+  test_settings: {
+    default: {},
+    chrome: {
+      desiredCapabilities: {
+        browser: 'chrome'
+      }
+    },
+    firefox: {
+      desiredCapabilities: {
+        browser: 'firefox'
+      }
+    },
+    safari: {
+      desiredCapabilities: {
+        browser: 'safari'
+      }
+    },
+    ie: {
+      desiredCapabilities: {
+        browser: 'internet explorer'
+      }
+    }
+  }
+};
+
+// Code to support common capabilites
+for (const i in nightwatch_config.test_settings) {
+  const config = nightwatch_config.test_settings[i];
+  config.selenium_host = nightwatch_config.selenium.host;
+  config.selenium_port = nightwatch_config.selenium.port;
+  config.desiredCapabilities = config.desiredCapabilities || {};
+  for (const j in nightwatch_config.common_capabilities) {
+    config.desiredCapabilities[j] =
+      config.desiredCapabilities[j] || nightwatch_config.common_capabilities[j];
+  }
+}
+
+module.exports = nightwatch_config;
