2da2929ae37fb0fa1f9048b3c5d00977817d9454
ArturJS <nizamutdin.art@gmail.com>
2019-03-18 07:58:33 +0400

    Added more unit tests for 'info' modal

--- src/modal-dialog/__tests__/modal-dialog.test.jsx
+++ src/modal-dialog/__tests__/modal-dialog.test.jsx
@@ -29,8 +29,14 @@ jest.mock('react-transition-group', () => ({
   )
 }));
 
+const flushPromises = () => new Promise(resolve => setImmediate(resolve));
+
 describe('<ModalDialog />', () => {
   describe('type `info`', () => {
+    afterEach(async () => {
+      await modalService.closeAll();
+    });
+
     it('should match snapshot', () => {
       modalService.info({ title: 'Info title', body: 'Info body' });
 
@@ -45,6 +51,46 @@ describe('<ModalDialog />', () => {
       const wrapper = mount(<ModalDialog />);
 
       expect(wrapper.find('.rmb-modal-content').exists()).toBe(true);
+    });
+
+    it('should be closed', async () => {
+      let isClosed = false;
+
+      modalService
+        .info({ title: 'Info title', body: 'Info body' })
+        .result.then(() => {
+          isClosed = true;
+        });
+
+      const wrapper = mount(<ModalDialog />);
+
+      wrapper.find('.rmb-btn-ok').simulate('click');
+      await flushPromises();
+
+      expect(wrapper.find('.rmb-modal-content').exists()).toBe(false);
+      expect(isClosed).toBe(true);
+    });
+
+    it('should be dismissed', async () => {
+      let isDismissed = false;
+
+      modalService
+        .info({
+          title: 'Info title',
+          body: 'Info body',
+          throwCancelError: true
+        })
+        .result.catch(() => {
+          isDismissed = true;
+        });
+
+      const wrapper = mount(<ModalDialog />);
+
+      wrapper.find('.rmb-close').simulate('click');
+      await flushPromises();
+
+      expect(wrapper.find('.rmb-modal-content').exists()).toBe(false);
+      expect(isDismissed).toBe(true);
     });
   });
 });
