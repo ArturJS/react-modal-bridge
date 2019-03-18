4c9b7e3a7978e9982d6972573a3802432dfa07c3
ArturJS <nizamutdin.art@gmail.com>
2019-03-18 08:13:52 +0400

    Added unit tests for 'confirm' modal

--- src/modal-dialog/__tests__/modal-dialog.test.jsx
+++ src/modal-dialog/__tests__/modal-dialog.test.jsx
@@ -32,6 +32,90 @@ jest.mock('react-transition-group', () => ({
 const flushPromises = () => new Promise(resolve => setImmediate(resolve));
 
 describe('<ModalDialog />', () => {
+  describe('type `confirm`', () => {
+    afterEach(async () => {
+      await modalService.closeAll();
+    });
+
+    it('should match snapshot', () => {
+      modalService.confirm({ title: 'Confirm title', body: 'Confirm body' });
+
+      const tree = renderer.create(<ModalDialog />).toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('should contain modal content', () => {
+      modalService.confirm({ title: 'Confirm title', body: 'Confirm body' });
+
+      const wrapper = mount(<ModalDialog />);
+
+      expect(wrapper.find('.rmb-modal-content').exists()).toBe(true);
+    });
+
+    it('should be closed', async () => {
+      let isClosed = false;
+
+      modalService
+        .confirm({ title: 'Confirm title', body: 'Confirm body' })
+        .result.then(() => {
+          isClosed = true;
+        });
+
+      const wrapper = mount(<ModalDialog />);
+
+      wrapper.find('.rmb-btn-ok').simulate('click');
+      await flushPromises();
+
+      expect(wrapper.find('.rmb-modal-content').exists()).toBe(false);
+      expect(isClosed).toBe(true);
+    });
+
+    it('should be dismissed by clicking [Cancel] button', async () => {
+      let isDismissed = false;
+
+      modalService
+        .confirm({
+          title: 'Confirm title',
+          body: 'Confirm body',
+          throwCancelError: true
+        })
+        .result.catch(() => {
+          isDismissed = true;
+        });
+
+      const wrapper = mount(<ModalDialog />);
+
+      wrapper.find('.rmb-btn-cancel').simulate('click');
+      await flushPromises();
+
+      expect(wrapper.find('.rmb-modal-content').exists()).toBe(false);
+      expect(isDismissed).toBe(true);
+    });
+
+    it('should be dismissed by clicking [X] button', async () => {
+      let isDismissed = false;
+
+      modalService
+        .confirm({
+          title: 'Confirm title',
+          body: 'Confirm body',
+          throwCancelError: true
+        })
+        .result.catch(() => {
+          isDismissed = true;
+        });
+
+      const wrapper = mount(<ModalDialog />);
+
+      wrapper.find('.rmb-close').simulate('click');
+      await flushPromises();
+
+      expect(wrapper.find('.rmb-modal-content').exists()).toBe(false);
+      expect(isDismissed).toBe(true);
+    });
+  });
+
   describe('type `info`', () => {
     afterEach(async () => {
       await modalService.closeAll();
