6429ab9e384e0a9122a22aad62ef96d02dcf87ab
ArturJS <nizamutdin.art@gmail.com>
2019-03-29 08:16:25 +0400

    Increase wait timeout up to 25 seconds in order to fix e2e tests in slow TravisCI env

--- e2e-tests/conf/local.conf.js
+++ e2e-tests/conf/local.conf.js
@@ -13,7 +13,7 @@ const nightwatchConfig = {
   test_settings: {
     default: {
       globals: {
-        waitForConditionTimeout: 10000
+        waitForConditionTimeout: 25000
       },
       desiredCapabilities: {
         build: 'nightwatch-browserstack',
